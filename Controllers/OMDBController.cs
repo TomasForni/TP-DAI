6ee959bd9cd40f0c2b1b68ed3c48fb0911ec009f
TOMÁS MARCOS FORNI <47294844@ort>
2023-03-21 08:52:28 -0300

    movies

---
+++ Controllers/OMDBController.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.Linq.Expressions;
+using System.IO;
+namespace Pizzas.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OMDBController : ControllerBase
+    {
+
+    }
+}
